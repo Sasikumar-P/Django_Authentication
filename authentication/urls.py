eca8d223be3060d2f8616d2ab0a1b24ff9575ebd
comrade <us@nithin-Inspiron-N4010.(none)>
2016-01-15 15:51:07 +0530

    First commit

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('security.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+]
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ security/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from security import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+]
